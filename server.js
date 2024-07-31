d820553afb7ec4efbf9905f807f1572102628c7d
Shazaniyu55 <shazaniyu@gmail.com>
2024-07-31 13:21:50 +0100

    new

--- server.js
+++ server.js
@@ -14,33 +14,6 @@ const uploadCourseRoute = require('./routes/upload.route')
 require('dotenv').config();
 
 
-// const flw = new Flutterwave(process.env.PUBLIC_KEY, process.env.SECRETE_KEY );
-
-
-// const initTransfer = async()=>{
-//   try {
-//     const payload = {
-//     "account_bank": "044", //This is the recipient bank code. Get list here :https://developer.flutterwave.com/v3.0/reference#get-all-banks
-//     "account_number": "0812601495",
-//     "amount": 300,
-//     "narration": "Akhlm Pstmn Trnsfr xx007",
-//     "currency": "NGN",
-//     "reference": "akhlm-pstmnpyt-r02ens007_PMCKDU_1", //This is a merchant's unique reference for the transfer, it can be used to query for the status of the transfer
-//     "callback_url": "https://www.flutterwave.com/ng/",
-//     "debit_currency": "NGN"
-// }
-
-//     const response = await flw.Transfer.initiate(payload)
-//     console.log(response);
-// } catch (error) {
-//     console.log(error)
-// }
-
-// }
-
-// initTransfer()
-
-
 //callback function to authenticate users
 function isAuthenticated(req, res, next) {
   if (req.session && req.session.user) {
@@ -97,9 +70,18 @@ app.use(session({
 app.use('/users', userRoutes);
 app.use('/admin', uploadCourseRoute)
 
-//host the home page of the index.ejs page
+
 app.get('/', (req, res)=>{
-res.render('index',{ user: req.session.user})
+  res.render('index',{ user: req.session.user})
+});
+
+
+//host the home page of the index.ejs page
+app.get('/courses', (req, res)=>{
+  conn.query(`SELECT * FROM courses`, (err, result)=>{
+    res.render('viewcourses',{results:result, user: req.session.user})
+})
+  
 });
 
 //to get the course id from the mysql database.
